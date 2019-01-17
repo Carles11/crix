8d2cb716796071f41623cfe1e6720d2e935c9219
Carles11 <c.delriofrances@gmail.com>
2019-01-17 18:45:03 +0100

    second commit

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -5,7 +5,7 @@ import mainImage from "../assets/images/1.jpg"
 const Landing = () => {
     return ( 
    <div> 
-        <h1>Landing</h1>
+        <h1>Landing super</h1>
         <img src={mainImage} alt="cervantes" />
     </div>
 )
