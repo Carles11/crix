d311c89cacc2d999d461d79b780e8a2c4c259d97
Carles11 <c.delriofrances@gmail.com>
2019-01-17 21:53:10 +0100

    change port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = process.env.NODE_ENV || 3000
 
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.static(path.join(__dirname, "dist")))
@@ -9,7 +10,7 @@ app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"))
 } )
 
-app.listen(3000,() => {
-    console.log("listening on port 3000")
+app.listen(port,() => {
+    console.log(`listening on port ${port}`)
 })
 
