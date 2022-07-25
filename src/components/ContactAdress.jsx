008967e148d2f70ce0c6f77f57537fb22f3385bd
Carles <carles@crix.design>
2022-07-25 18:17:57 -0400

    contactAddress refactored

--- src/components/ContactAdress.jsx
+++ src/components/ContactAdress.jsx
@@ -1,38 +1,11 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
-import styled from 'styled-components'
 
-const HighlightMe = styled.h4`
-  background-color: black;
-  display: inline;
-`
-// const StyledLink = styled(Link)`
-//   color: palevioletred;
-//   font-weight: bold;
-//   mix-blend-mode: difference;
-// `;
-const Article = styled.article`
-  text-align: left;
-  line-height: 1.3;
-  margin-left: 20%;
-  margin-right: 20%;
-  padding-bottom: 6%;
-  max-width: 60%;
-  color: silver;
-  font-size: 30px;
-
-  @media screen and (max-width: 600px) {
-    font-size: 24px;
-  }
-  @media screen and (min-width: 801px) {
-    font-size: 27px;
-  }
-`
+import '../css/contactAddress.css'
 
 const ContactAdress = () => {
   return (
-    <Article>
-      <HighlightMe>
+    <div className="address-content">
+      <h4 className="address-highlight">
         Elbestr. 15
         <br />
         60329 Frankfurt am Main
@@ -42,8 +15,8 @@ const ContactAdress = () => {
         Tel.: 01782871785
         <br />
         E-Mail: <a href="mailto:carles@crix.design"> carles@crix.design</a>
-      </HighlightMe>
-    </Article>
+      </h4>
+    </div>
   )
 }
 
