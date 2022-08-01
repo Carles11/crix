4327a0c53ba4c23b34844cd59a11e9b56f7625ae
Carles <carles@crix.design>
2022-08-01 17:30:56 -0400

    refactored finished

--- src/components/InnerPageHeader.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const InnerPageHeader = (pageTitle) => {
-  return <h1>{pageTitle}</h1>
-}
-
-export default InnerPageHeader
--- src/components/NavigationMobile.jsx
+++ src/components/NavigationMobile.jsx
@@ -4,47 +4,23 @@ import { Link } from 'react-router-dom'
 
 import CrossMenu from './CrossMenu'
 
-const StyledMenu = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: end;
-  text-align: right;
-  padding-right: 20px;
-  color: silver;
-  text-decoration: none;
-  cursor: pointer;
-  line-height: 0.1;
-`
-
-const StyledLink = styled(Link)`
-  color: #0a6284;
-  text-decoration: none;
-  white-space: nowrap;
-  cursor: pointer;
-`
-
-const CrossPosition = styled.div`
-  margin-left: 120px;
-  margin-top: 40px;
-`
-
 class NavigationMobile extends React.Component {
   render() {
     return (
-      <StyledMenu>
-        <CrossPosition>
+      <div className="mobile-menu">
+        <div className="mobile-menu-cross">
           <CrossMenu handleClose={this.props.handleClose} />
-        </CrossPosition>
-        <StyledLink to="/whatandwithwho">
+        </div>
+        <Link className="mobile-menu-link" to="/whatandwithwho">
           <h4>Qué hago y con quién trabajo</h4>
-        </StyledLink>
-        <StyledLink to="/about-us">
+        </Link>
+        <Link className="mobile-menu-link" to="/about-us">
           <h4>Sobre Carles</h4>
-        </StyledLink>
-        <StyledLink to="/contact">
+        </Link>
+        <Link className="mobile-menu-link" to="/contact">
           <h4>Contacta</h4>
-        </StyledLink>
-      </StyledMenu>
+        </Link>
+      </div>
     )
   }
 }
