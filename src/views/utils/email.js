36a29f1bffcf6b747bebb01649503f59cbd6760d
Carles <carles@crix.design>
2022-08-20 04:27:00 -0400

    sendinEmail working

--- src/views/utils/email.js
+++ src/views/utils/email.js
@@ -2,7 +2,6 @@ import emailjs, { init } from 'emailjs-com'
 import Swal from 'sweetalert2'
 
 const emailSend = (data) => {
-  console.log('--------> emailSend.emailSendemailSend DATA', data)
   const serviceID = 'default_service'
   const templateID = 'template_rvgqsba'
   const showName = data.name?.split(' ')[0]
@@ -10,10 +9,9 @@ const emailSend = (data) => {
 
   emailjs.send(serviceID, templateID, data).then(
     (result) => {
-      console.log('--------> emailjs.sending DATA', data)
       Swal.fire({
         title: `Gracias, ${showName}!`,
-        text: `Gracias, Tu petición ha sido enviada. En breve nos pondremos en contacto contigo. ${result.text}`,
+        text: `Gracias, tu petición ha sido enviada. En breve nos pondremos en contacto contigo. ${result.text}`,
         icon: 'success',
         showConfirmButton: false,
         showCancelButton: true,
@@ -21,7 +19,6 @@ const emailSend = (data) => {
       })
     },
     (error) => {
-      console.log('--------> emailjs.error DATA', data)
       Swal.fire({
         title: `Lo siento, ${showName}:`,
         text: `Parece que algo no ha funcionado.(${error}) Inténtalo de nuevo por favor. `,
