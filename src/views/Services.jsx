70b92a482c0080ad0ba92e1340ece925095ae660
carlos <carlos@3pace.dev>
2023-05-13 07:30:46 -0400

    all good

--- src/views/Services.jsx
+++ src/views/Services.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled from 'styled-components'
 // import PropTypes from 'prop-types';
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import QuotationForm from "../components/QuotationForm";
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import QuotationForm from '../components/QuotationForm'
 
 const Title = styled.h1`
   font-family: Montserrat;
@@ -12,7 +12,7 @@ const Title = styled.h1`
   text-align: center;
   margin-top: 50px;
   padding-top: 125px;
-`;
+`
 const Subtitle = styled.h2`
   font-family: Montserrat;
   font-size: 2.2em;
@@ -20,13 +20,13 @@ const Subtitle = styled.h2`
   margin: auto;
   padding-top: 4%;
   padding-bottom: 2%;
-`;
+`
 const Bg_pic_block = styled.section`
   min-height: auto;
   width: 100%;
   color: white;
 
-  background-image: url("https://res.cloudinary.com/dssldws2k/image/upload/v1552013085/CRiF/BG_code_javascript_FILTRD.png");
+  background-image: url('https://res.cloudinary.com/dssldws2k/image/upload/v1552013085/CRiF/BG_code_javascript_FILTRD.png');
   background-position: bottom center;
   background-attachment: scroll;
 
@@ -42,7 +42,7 @@ const Bg_pic_block = styled.section`
     background-repeat: no-repeat;
     background-attachment: fixed;
   }
-`;
+`
 const White_block = styled.section`
   background-color: white;
   color: #696969;
@@ -53,14 +53,12 @@ const White_block = styled.section`
   @media only screen and (max-width: 600px) {
     min-height: auto;
     width: 80%;
-
   }
   @media only screen and (max-width: 800px) {
     min-height: auto;
     width: 75%;
-
   }
-`;
+`
 const Article = styled.article`
   font-family: Montserrat;
   font-size: 1.4em;
@@ -86,7 +84,7 @@ const Article = styled.article`
     letter-spacing: 1px;
     line-height: 1.6em;
   }
-`;
+`
 const Article_w = styled.article`
   font-family: Montserrat;
   font-size: 1.4em;
@@ -112,45 +110,50 @@ const Article_w = styled.article`
     letter-spacing: 1px;
     line-height: 1.6;
   }
-`;
-
+`
 
 const Services = () => {
-    return (
-      <div>
-        <Header />
-        <Title>SERVICES</Title>   
-            <White_block>
-                <Subtitle>Web development</Subtitle>
-                <Article>
-                You need a website. Your budget is limited but nonetheless you expect a professional website to impress your clients
-                and you want to leave a door open to scalate the features of your website as your project grows.
-         
-                We use the latest technologies when developing our applications. 
-                All the code we write for you is personally optimized to your necessities, 
-                and it is very scalable. That way, if you start with a small project that covers your needs for now and you want to make it 
-                grow when the time is right, it will nicely grow. We don´t use any templates or web constructing systems. We just Javascript code from scratch for you.
-                </Article>
-            </White_block>
-            <Bg_pic_block>
-                <Subtitle>Server maintenance</Subtitle>
-                <Article_w>
-                Besides coding you a tailor made website, we offer you server maintenance and domain buy.
-                Leave us the job of maintaining your server and let us take care of the domain you like. We update all our servers regularly and take care that your website is always on-line.
-                </Article_w>
-            </Bg_pic_block>
-            <White_block>
-                <Subtitle>What about prices?</Subtitle>
-                <Article>
-                    We offer you fixed prices for your website. Tell us what features you need in your site and
-                    we will give you that fair price you have in mind. 
-                </Article>
-            </White_block>
-            <QuotationForm />
-            <Footer />
-      </div>
-    );
-};
+  return (
+    <div>
+      <Header />
+      <Title>SERVICES</Title>
+      <White_block>
+        <Subtitle>Web development</Subtitle>
+        <Article>
+          You need a website. Your budget is limited but nonetheless you expect
+          a professional website to impress your clients and you want to leave a
+          door open to scalate the features of your website as your project
+          grows. We use the latest technologies when developing our
+          applications. All the code we write for you is personally optimized to
+          your necessities, and it is very scalable. That way, if you start with
+          a small project that covers your needs for now and you want to make it
+          grow when the time is right, it will nicely grow. We don´t use any
+          templates or web constructing systems. We just Javascript code from
+          scratch for you.
+        </Article>
+      </White_block>
+      <Bg_pic_block>
+        <Subtitle>Server maintenance</Subtitle>
+        <Article_w>
+          Besides coding you a tailor made website, we offer you server
+          maintenance and domain buy. Leave us the job of maintaining your
+          server and let us take care of the domain you like. We update all our
+          servers regularly and take care that your website is always on-line.
+        </Article_w>
+      </Bg_pic_block>
+      <White_block>
+        <Subtitle>What about prices?</Subtitle>
+        <Article>
+          We offer you fixed prices for your website. Tell us what features you
+          need in your site and we will give you that fair price you have in
+          mind.
+        </Article>
+      </White_block>
+      <QuotationForm />
+      <Footer />
+    </div>
+  )
+}
 
 // Services.propTypes = {
 //   DIC: PropTypes.shape({
@@ -158,5 +161,4 @@ const Services = () => {
 //   }).isRequired,
 // };
 
-
-export default Services;
+export default Services
