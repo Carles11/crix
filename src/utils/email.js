21a049aa3cf3de9b50bc9d6bd84c773287cdc9a0
carlos <c.delriofrances@gmail.com>
2024-10-02 17:07:27 -0300

    env temp fix

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,12 +1,14 @@
 import emailjs, { init } from 'emailjs-com'
 import Swal from 'sweetalert2'
-
+REACT_APP_SERVICE_ID = service_1xubnpl
+REACT_APP_TEMPLATE_ID = template_rvgqsba
+REACT_APP_EMAILJS_PUBLIC_KEY = user_5HJvI2zi5tRGkdRn7cAvt
 // Initialize EmailJS with public key
-init(process.env.REACT_APP_EMAILJS_PUBLIC_KEY)
+init(process.env.REACT_APP_EMAILJS_PUBLIC_KEY || 'user_5HJvI2zi5tRGkdRn7cAvt')
 
 const emailSend = (data, DIC, resetForm) => {
-  const serviceID = process.env.REACT_APP_SERVICE_ID
-  const templateID = process.env.REACT_APP_TEMPLATE_ID
+  const serviceID = process.env.REACT_APP_SERVICE_ID || 'service_1xubnpl'
+  const templateID = process.env.REACT_APP_TEMPLATE_ID || 'template_rvgqsba'
   const showName = data.name?.split(' ')[0]
 
   console.log({ serviceID, templateID, data })
